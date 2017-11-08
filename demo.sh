784b4afa2cf4f4d666f2c1285a1861ae75f51aa2
Nagarjuna Kumar <nagarjuna.kumar@duedil.com>
2017-11-08 12:00:25 +0000

    Updated demo script with tlog

--- demo.sh
+++ demo.sh
@@ -3,14 +3,14 @@
 
 source tinylogger.bash
 
-logger debug "first line of script"
-logger info "Starting script $0"
-logger debug "another debug statement"
+tlog debug "first line of script"
+tlog info "Starting script $0"
+tlog debug "another debug statement"
 
-logger info "Current time is $( date )"
-logger warn "This is a scary warning"
-logger debug "do you like these debug statements?"
-logger error "Something terrible has happened"
+tlog info "Current time is $( date )"
+tlog warn "This is a scary warning"
+tlog debug "do you like these debug statements?"
+tlog error "Something terrible has happened"
 
-logger debug "end of script"
-logger info "Script completed, go home"
+tlog debug "end of script"
+tlog info "Script completed, go home"
